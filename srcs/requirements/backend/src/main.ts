1b922c9752bbef4622bd4d654436c96a5d8bee64
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-09-27 11:50:37 +0100

    add: configure nestjs port

--- srcs/requirements/backend/src/main.ts
+++ srcs/requirements/backend/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // TODO: use env variable (nestjs config module) to make port configurable
-  await app.listen(3000);
+  await app.listen(process.env.NESTJS_PORT || 3000);
 }
 bootstrap();
