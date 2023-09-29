01902d0423259adaca515afe8fc87519662b07be
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-09-29 18:11:49 +0100

    Refactor seed script to create a friendship between two users (still not working)

--- srcs/requirements/backend/prisma/seed.ts
+++ srcs/requirements/backend/prisma/seed.ts
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient();
 
 async function clean() {
-  await prisma.user.deleteMany();
+  // await prisma.user.deleteMany();
 }
 
 async function generateRandomUser() {
@@ -12,26 +12,51 @@ async function generateRandomUser() {
     data: {
       username: faker.internet.userName(),
       email: faker.internet.email(),
-      password: faker.internet.password(),
-      avatar: faker.image.avatar(),
+      // password: faker.internet.password(),
+      // avatar: faker.image.avatar(),
+      // twoFactorEnabled: faker.datatype.boolean(),
+      // twoFactorSecret: faker.string.hexadecimal(),
     },
   });
 
-  return prisma.user.findUnique({
-    where: {
-      id: user.id,
+  // return prisma.user.findUnique({
+  //   where: {
+  //     id: user.id,
+  //   },
+  // });
+  return user;
+}
+
+async function createFriendship(userId, friendUserId) {
+  const friend = await prisma.friend.create({
+    data: {
+      user: {
+        connect: { id: friendUserId },
+      },
     },
   });
+  return friend;
 }
 
 async function main() {
   await clean();
 
-  const users = await Promise.all(
-    Array.from({ length: 10 }).map(() => generateRandomUser()),
-  );
+  // const users = await Promise.all(
+  //   Array.from({ length: 10 }).map(() => generateRandomUser()),
+  // );
 
-  console.log('Users created ✅');
+  const user1 = await generateRandomUser();
+
+  console.log('User1 created ✅');
+  console.log({ user1 });
+
+  const user2 = await generateRandomUser();
+  console.log('User2 created ✅');
+  console.log({ user2 });
+
+  const friend = await createFriendship(user1.id, user2.id);
+  console.log('Friendship created ✅');
+  console.log({ friend });
 }
 
 main()
