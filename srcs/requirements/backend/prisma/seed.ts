9cf96b53fd30f5fd34677d872b8d959ff09df911
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-09-30 11:34:21 +0100

    add: creating 10 new random users for testing

--- srcs/requirements/backend/prisma/seed.ts
+++ srcs/requirements/backend/prisma/seed.ts
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient();
 
 async function clean() {
-  // await prisma.user.deleteMany();
+  await prisma.user.deleteMany();
 }
 
 async function generateRandomUser() {
@@ -12,51 +12,21 @@ async function generateRandomUser() {
     data: {
       username: faker.internet.userName(),
       email: faker.internet.email(),
-      // password: faker.internet.password(),
-      // avatar: faker.image.avatar(),
+      password: faker.internet.password(),
+      avatar: faker.image.avatar(),
       // twoFactorEnabled: faker.datatype.boolean(),
       // twoFactorSecret: faker.string.hexadecimal(),
     },
   });
 
-  // return prisma.user.findUnique({
-  //   where: {
-  //     id: user.id,
-  //   },
-  // });
   return user;
 }
 
-async function createFriendship(userId, friendUserId) {
-  const friend = await prisma.friend.create({
-    data: {
-      user: {
-        connect: { id: friendUserId },
-      },
-    },
-  });
-  return friend;
-}
-
 async function main() {
-  await clean();
-
-  // const users = await Promise.all(
-  //   Array.from({ length: 10 }).map(() => generateRandomUser()),
-  // );
-
-  const user1 = await generateRandomUser();
-
-  console.log('User1 created ✅');
-  console.log({ user1 });
-
-  const user2 = await generateRandomUser();
-  console.log('User2 created ✅');
-  console.log({ user2 });
-
-  const friend = await createFriendship(user1.id, user2.id);
-  console.log('Friendship created ✅');
-  console.log({ friend });
+  // await clean();
+  for (let i = 0; i < 10; i++) {
+    await generateRandomUser();
+  }
 }
 
 main()
