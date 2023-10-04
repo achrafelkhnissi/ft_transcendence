ce94a11fad4ff8491617e5d8b6cd4f5d91d15e82
Fathiyat olatokunbo Jamia <fathjami@e3r10p6.1337.ma>
2023-10-04 18:33:11 +0100

    fix padding

--- srcs/requirements/frontend/src/app/components/SearchInput.tsx
+++ srcs/requirements/frontend/src/app/components/SearchInput.tsx
@@ -2,10 +2,10 @@ import { LuSearch } from "react-icons/lu";
 
 const SearchInput = () => {
   return (
-    <div className="bg-[#424283] border rounded-full relative w-[400px] h-full py-1 px-2">
+    <div className="bg-[#424283] border rounded-full relative w-[400px]  py-1 px-2">
       <input
         type="text"
-        className="w-full h-10 pl-2 bg-transparent outline-none"
+        className="w-full py-1 px-2 bg-transparent outline-none"
         placeholder="Search something..."
       />
       <button className="absolute top-0 bottom-0 right-4 ">
