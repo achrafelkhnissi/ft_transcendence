dd3b417963980cf48b63513230e2a61e51db8bb4
Fathiyat olatokunbo Jamia <fathjami@e3r8p10.1337.ma>
2023-10-05 18:41:23 +0100

    add Logo component

--- srcs/requirements/frontend/src/app/components/Header.tsx
+++ srcs/requirements/frontend/src/app/components/Header.tsx
@@ -1,11 +1,12 @@
 import SearchInput from "./SearchInput";
 import Notifications from "./Notifications";
 import Avatar from "./Avatar";
+import Logo from "./Logo";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-3 py-[0.6rem] bg-[#39396F] shadow-xl  shadow-gray-900/20 items-center">
-      <div className="text-white">LOGO</div>
+      <Logo />
       <SearchInput />
       <div className="flex gap-4">
         <Notifications />
