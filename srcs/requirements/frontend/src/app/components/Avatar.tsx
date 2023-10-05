4078e4f0439dcedacb2bc2cca43a80b4c6575ea9
Fathiyat olatokunbo Jamia <fathjami@e3r8p10.1337.ma>
2023-10-05 15:30:55 +0100

    add avatar component

--- srcs/requirements/frontend/src/app/components/Avatar.tsx
+++ srcs/requirements/frontend/src/app/components/Avatar.tsx
@@ -1,5 +1,36 @@
+import { FaUserAlt } from "react-icons/fa";
+import Image from "next/image";
+
 const Avatar = () => {
-  return <div className="text-white">Avatar</div>;
+  return (
+    <div
+      className={`h-[2rem] 
+    w-[2rem] 
+    bg-gray-400/60
+     rounded-lg 
+     flex 
+     justify-center 
+     items-center 
+     outline 
+     outline-2 
+     outline-offset-[1.5px]
+     outline-gray-400/60`}
+    >
+      {/* <FaUserAlt
+        style={{
+          color: "white",
+          opacity: 0.6,
+        }}
+      /> */}
+      <Image
+        src="/images/fathjami.jpeg"
+        alt="user"
+        width={40}
+        height={40}
+        className="rounded-lg"
+      />
+    </div>
+  );
 };
 
 export default Avatar;
