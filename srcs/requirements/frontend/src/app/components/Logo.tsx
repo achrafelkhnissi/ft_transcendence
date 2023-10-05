b4205ae938207b5ab24fed9f3aa41e639edafeae
Fathiyat olatokunbo Jamia <fathjami@e3r8p10.1337.ma>
2023-10-05 18:42:03 +0100

    logo component

---
+++ srcs/requirements/frontend/src/app/components/Logo.tsx
@@ -0,0 +1,15 @@
+import Hourglass from "./Hourglass";
+import { pixelifySans } from "../fonts";
+
+const Logo = () => {
+  return (
+    <div
+      className={`flex text-4xl items-center text-[#93C4FD] -gap-6 ${pixelifySans.className}  drop-shadow-[0_3px_4px_rgba(147,196,253,0.9)]`}
+    >
+      PON
+      <Hourglass color="" width="2rem" height="2rem" />
+    </div>
+  );
+};
+
+export default Logo;
