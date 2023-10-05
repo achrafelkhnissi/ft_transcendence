83b4d5bb79928993b34e2cb96f7ce3915bceb9a6
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-10-05 20:39:22 +0100

    rm: user

--- srcs/requirements/backend/src/app.module.ts
+++ srcs/requirements/backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, PrismaModule],
+  imports: [UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
