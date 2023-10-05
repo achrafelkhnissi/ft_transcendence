fdb333698a315f4816f48d414cfce426c93050a6
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-10-05 20:39:06 +0100

    rename: from user to users

---
+++ srcs/requirements/backend/src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  // TODO: Check if 'readonly' is needed
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createUserDto: CreateUserDto) {
+    createUserDto;
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    updateUserDto;
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
