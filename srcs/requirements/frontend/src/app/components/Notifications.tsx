7daf4f0fc9ce7d6f795000fa0ef8e752bd7409ba
Fathiyat olatokunbo Jamia <fathjami@e3r8p10.1337.ma>
2023-10-05 14:43:11 +0100

    add ping animation to icon

--- srcs/requirements/frontend/src/app/components/Notifications.tsx
+++ srcs/requirements/frontend/src/app/components/Notifications.tsx
@@ -1,7 +1,22 @@
+import { GrNotification } from "react-icons/gr";
+import { IoNotificationsOutline } from "react-icons/io5";
+
 const Notifications = () => {
   return (
-    <div>
-      <h1 className="text-white">Notifications</h1>
+    <div className=" self-center relative">
+      <IoNotificationsOutline
+        color="red"
+        style={{
+          color: "white",
+          opacity: 0.6,
+          width: "1.7rem",
+          height: "1.7rem",
+        }}
+      />
+      <div className="absolute  top-[0.1rem] right-[0.15rem]  w-[0.6rem] h-[0.6rem]">
+        <span className="animate-ping absolute h-full w-full rounded-full bg-white/95 "></span>
+        <span className="bg-[#6257FE] h-full w-full absolute rounded-full "></span>
+      </div>
     </div>
   );
 };
