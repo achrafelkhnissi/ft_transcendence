540023b1481b1f3231e7629f216a782e8e4bc4cd
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-10-05 20:54:05 +0100

    add; route for help msg

--- srcs/requirements/backend/src/app.controller.ts
+++ srcs/requirements/backend/src/app.controller.ts
@@ -5,7 +5,12 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get(['/', 'help'])
+  getHelp(): object {
+    return this.appService.getHelp();
+  }
+
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
   }
--- srcs/requirements/backend/src/app.service.ts
+++ srcs/requirements/backend/src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
+const HELP = {
+  'GET /': 'Get this help message',
+  users: {
+    'GET /users': 'Get all users',
+    'GET /users/:id': 'Get a user by id',
+    'POST /users': 'Create a user',
+    'PUT /users/:id': 'Update a user by id',
+    'DELETE /users/:id': 'Delete a user by id',
+  },
+};
+
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getHelp(): object {
+    return HELP;
   }
 }
