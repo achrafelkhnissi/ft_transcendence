5e82c0ef935d149321f5d9f007fd512e51caf300
Fathiyat olatokunbo Jamia <fathjami@e3r8p10.1337.ma>
2023-10-05 11:44:22 +0100

    resize icon, clean input, search query

--- srcs/requirements/frontend/src/app/components/SearchInput.tsx
+++ srcs/requirements/frontend/src/app/components/SearchInput.tsx
@@ -1,21 +1,42 @@
+"use client";
+
 import { LuSearch } from "react-icons/lu";
+import { CiSearch } from "react-icons/ci";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const SearchInput = () => {
+  const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [input, setInput] = useState("");
+
+  const onSearch = () => {
+    setInput(""); // clear input
+    const encodedQuery = encodeURIComponent(searchQuery);
+    router.push(`/search?query=${encodedQuery}`);
+    console.log("searching for", searchQuery);
+  };
+
   return (
-    <div className="bg-[#424283] border rounded-full relative w-[400px]  py-1 px-2">
+    <div className="bg-[#424283] border-[0.09rem] border-white/50 rounded-full relative w-[400px]  py-1 px-2">
       <input
         type="text"
-        className="w-full py-1 px-2 bg-transparent outline-none"
-        placeholder="Search something..."
+        value={input}
+        className="w-full py-[0.3rem] px-2 bg-transparent outline-none opacity-4 text-light text-sm text-white font-extralight placeholder:italic"
+        placeholder="search something..."
+        onChange={(event) => {
+          setSearchQuery(event.target.value);
+          setInput(event.target.value);
+        }}
       />
-      <button className="absolute top-0 bottom-0 right-4 ">
-        <LuSearch
+      <button className="absolute top-0 bottom-0 right-3" onClick={onSearch}>
+        <CiSearch
           style={{
             color: "white",
-            opacity: 0.7,
-            fontSize: "200px",
+            opacity: 0.6,
+            width: "1.6rem",
+            height: "1.6rem",
           }}
-          size={50}
         />
       </button>
     </div>
