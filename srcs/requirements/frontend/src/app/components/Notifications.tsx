673a5d7b01ff58420969bd1ef260c6d49f3a4168
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-10-07 18:34:05 +0100

    add hover style

--- srcs/requirements/frontend/src/app/components/Notifications.tsx
+++ srcs/requirements/frontend/src/app/components/Notifications.tsx
@@ -1,22 +1,25 @@
 import { GrNotification } from "react-icons/gr";
 import { IoNotificationsOutline } from "react-icons/io5";
+import { IoIosNotificationsOutline } from "react-icons/io";
 
 const Notifications = () => {
   return (
-    <div className=" self-center relative">
-      <IoNotificationsOutline
+    <div className="hover:bg-white/10 rounded-lg h-9 w-9 flex justify-center ">
+    <div className=" relative self-center ">
+      <IoIosNotificationsOutline
         color="red"
         style={{
           color: "white",
           opacity: 0.6,
-          width: "1.7rem",
-          height: "1.7rem",
+          width: "1.9rem",
+          height: "1.9rem",
         }}
       />
-      <div className="absolute  top-[0.1rem] right-[0.15rem]  w-[0.6rem] h-[0.6rem]">
+      <div className="absolute  top-[0.22rem] right-[0.3rem]  w-[0.5rem] h-[0.5rem]">
         <span className="animate-ping absolute h-full w-full rounded-full bg-white/95 "></span>
         <span className="bg-[#6257FE] h-full w-full absolute rounded-full "></span>
       </div>
+    </div>
     </div>
   );
 };
