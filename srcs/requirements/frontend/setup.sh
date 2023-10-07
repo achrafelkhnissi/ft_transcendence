b21201e16ec5277509aa6f17d66c25699ff97784
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-10-07 13:01:12 +0100

    test

--- srcs/requirements/frontend/setup.sh
+++ srcs/requirements/frontend/setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker rm ${docker ps -a -q}
 docker build -t frontend .
 docker run --rm -it --mount type=bind,source="$(pwd)",target=/app -d -p 1337:1337 --name frontend frontend bash
-docker exec frontend sh -c "npm install"
 docker exec frontend sh -c "echo 'export NEXTJS_PORT=1337' >> ~/.bashrc && source ~/.bashrc"
+docker exec frontend sh -c "npm install && npm run dev"
+docker logs -f frontend
 
 # echo 'export NEXTJS_PORT=1337' >> ~/.bashrc && source ~/.bashrc
