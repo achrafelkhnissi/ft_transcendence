bbb279dcd24da90bf34bf579cc3235d7aa63d1e2
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-10-08 15:25:12 +0100

    add the sidebar and header components to the layout

--- srcs/requirements/frontend/src/app/layout.tsx
+++ srcs/requirements/frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Sidebar from "./components/layout/sidebar/Sidebar";
+import Header from "./components/layout/header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Sidebar />
+        <Header />
+        {children}
+        </body>
     </html>
   );
 }
