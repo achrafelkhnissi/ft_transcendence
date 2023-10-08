5fdff43cbb0143a168ba42af5533a232f13b8408
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-10-08 10:07:13 +0100

    later make the searchbar responsive

--- srcs/requirements/frontend/src/app/components/Header.tsx
+++ srcs/requirements/frontend/src/app/components/Header.tsx
@@ -8,26 +8,28 @@ import { useState, useEffect } from "react";
 import SmSearchInput from "./SmSearchinput";
 
 const Header = () => {
-  const [screenW, setScreenW] = useState(0);
+  // const [screenW, setScreenW] = useState(0); //TODO: fix this to be responsive
+  
+  // useEffect(() => {
+  //   setScreenW(window.innerWidth);
+  //   const handleWindowResize = () => {
+  //     setScreenW(window.innerWidth);
+  //   };
 
-  useEffect(() => {
-    const handleWindowResize = () => {
-      setScreenW(window.innerWidth);
-    };
+  //   window.addEventListener("resize", handleWindowResize);
 
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("resize", handleWindowResize);
+  //   };
+  // }, []);
 
   return (
     <div className="flex justify-between px-3 py-[0.6rem] bg-[#39396F] shadow-xl  shadow-gray-900/20 items-center">
       <div className={``}>
         <Logo />
       </div>
-      {screenW < 530 ? <SmSearchInput /> : <LgSearchInput />}
+      {/* {screenW < 530 ? <SmSearchInput /> : <LgSearchInput />} */}
+      <LgSearchInput />
       <div className={`flex gap-4`}>
         <Notifications />
         <Avatar />
