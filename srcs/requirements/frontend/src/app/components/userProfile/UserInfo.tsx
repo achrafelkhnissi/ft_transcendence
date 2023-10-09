5eff15aca7af050dedc2af38c3ed049031ba96f8
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-10-09 19:02:01 +0100

    fix div position

--- srcs/requirements/frontend/src/app/components/userProfile/UserInfo.tsx
+++ srcs/requirements/frontend/src/app/components/userProfile/UserInfo.tsx
@@ -1,12 +1,24 @@
+"use client"
+
 import Image from 'next/image'
+import Avatar from '../Avatar';
+import ProfileAvatar from './ProfileAvatar';
 
 const UserInfo = () => {
     return (
-    <div className="w-full p-6">
-        <div className="h-[300px] w-full rounded-xl border-2">
-            <Image src="/images/profilebg.png" width={1200} height={600} alt="photo" className="rounded-xl w-full h-full object-cover" />
+    <div className="w-full p-4">
+    <div className='w-full relative  h-[400px]'>
+        <div className="h-[350px] w-full rounded-2xl p-6">
+            <Image src="/images/profilebg.png" width={1200} height={600} alt="photo" className="rounded-[2.5rem] w-full h-full object-cover" />
+        </div>
+        <div className='absolute w-full h-28 bottom-8 rounded-[2.5rem] bg-[#20204A]/90'>
+            <div className='w-full relative h-full'>
+                <div className='absolute left-1/2 transform  -translate-x-1/2 -translate-y-1/2'>
+                    <ProfileAvatar />
+                </div>
+            </div>
         </div>
     </div>
+</div>
 )}
-
 export default UserInfo ;
