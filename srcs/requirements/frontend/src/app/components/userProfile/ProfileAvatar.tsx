21d7a431c2a34b7c3f003196fe0be1dbbaabc910
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-10-10 12:54:46 +0100

    add profile avatar

--- srcs/requirements/frontend/src/app/components/userProfile/ProfileAvatar.tsx
+++ srcs/requirements/frontend/src/app/components/userProfile/ProfileAvatar.tsx
@@ -2,17 +2,21 @@ import Image from "next/image";
 
 const ProfileAvatar = () => {
     return (<div className=" w-full h-full ">
-        <div className="">
+        <div className="rectangulaire-progress-bar rounded-2xl flex justify-center">
+        <div className="self-center">
             <Image
             src="/images/fathjami.jpeg" 
             width={40} 
             height={40} 
             alt="profile" 
             className={
-                `w-24 
-                h-24
-                rounded-3xl
+                `w-[6.1rem] 
+                h-[6.1rem]
+                rounded-xl
+
                 `}/>
+        </div>
+
         </div>
     </div>);
 };
