fea1a30a76a2da64d7201618063e174a9f4ac679
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-10-13 19:28:46 +0100

    implement the function to calculate the progress

--- srcs/requirements/frontend/src/app/components/userProfile/ProfileAvatar.tsx
+++ srcs/requirements/frontend/src/app/components/userProfile/ProfileAvatar.tsx
@@ -3,20 +3,118 @@ import { useEffect, useRef } from "react";
 
 const ProfileAvatar = () => {
     const progressRef = useRef<HTMLDivElement>(null);
-    const xp = 12;
+    const xp = 100; //user xp points
 
-    useEffect(() => {   
-        const totalLength = progressRef.current ? progressRef.current.offsetWidth * 2 + progressRef.current.offsetHeight * 2 : 0;
-        const side:number = totalLength / 4;
+    useEffect(() => {
+
+        const width = progressRef.current?.offsetWidth ;
+        const height = progressRef.current?.offsetHeight;
+        const totalLength =  width && height? width * 2 + height * 2 : 0;
+        const progressLength = (totalLength * xp) / 100;
+
         var backgroundSize = "";
         var backgroundPosition = "";
+        const halfBottomEdge = (totalLength / 4) / 2;
 
-        console.log("totalLength", totalLength);
-        console.log("side", side);
+        if (xp <= 12.5){ //the right bottom edge: why 12.5? each edge is 25% and the bottom edgde is devided by 2
 
+            backgroundSize = `  
+                            100% 10px,   
+                            10px 100%, 
+                            36% 10px, 
+                            ${xp * 2.4}% 10px, 
+                            10px 100% ,
+                            100% 100%;`
+            //the right bottom edge is (30% width so 30% equal 12.5% of the whole length so to calculate the percentage we need to multply it by (30 / 12.5) = 2.4
+            backgroundPosition = ` 
+                                -100px -100px, 
+                                -100px 0px, 
+                                0px -100px , 
+                                70px 100px, 
+                                -100px 0px, 
+                                0px 0px;`
+
+        } else if ( xp > 12.5 && xp <= 37.5){ // 12.5 < xp <= 12.5 + 25
         
+            backgroundSize = ` 
+                            100% 10px, 
+                            10px 100%, 
+                            36% 10px, 
+                            30% 10px, 
+                            10px 100% ,
+                            100% 100%;`
 
-    }, []);
+            backgroundPosition = ` 
+                                -100px -100px, 
+                                100px ${110 - (progressLength - halfBottomEdge)}px, 
+                                0px -100px , 
+                                70px 100px, 
+                                -100px -100px, 
+                                0px 0px;`
+
+            //why 110 - (progressLength - halgBottomEdge)? : hight of the border - (the length calculated - the pat already filled)
+        } else if ( xp > 37.5 && xp <= 62.5){
+
+            backgroundSize = ` 
+                            100% 10px, 
+                            10px 100%,
+                            30% 10px, 
+                            36% 10px, 
+                            10px 100% ,
+                            100% 100%;`
+
+            backgroundPosition = `
+                                 ${110 -( progressLength - halfBottomEdge - (totalLength / 4))}px 0px, 
+                                 100px 0px, 
+                                 0px -100px , 
+                                 70px 100px, 
+                                 -100px -100px, 
+                                 0px 0px;`
+
+        } else if (xp > 62.5 && xp <= 87.5){
+
+            backgroundSize = ` 
+                            100% 10px, 
+                            10px 100%,
+                            36% 10px, 
+                            30% 10px, 
+                            10px 100% ,
+                            100% 100%;`
+
+            backgroundPosition = ` 
+                                0px 0px, 
+                                100px 0px, 
+                                0px -100px , 
+                                70px 100px, 0px ${-110 + ( progressLength - halfBottomEdge - (totalLength / 2))}px, 
+                                0px 0px,
+                                0px 0px;`
+                                // width - the parts already filled
+        }
+        else{
+            backgroundSize = `
+                            100% 10px, 
+                            10px 100%, 
+                            ${(xp - 87.5) * 2.88 }% 10px, 
+                            30% 10px, 
+                            10px 100% ,
+                            100% 100%;`
+            //why 2.88? : 36 / 12.5 = 2.88
+            backgroundPosition = ` 
+                                0px 0px, 
+                                100px 0px, 
+                                0px 100px , 
+                                70px 100px, 
+                                0px 0px, 
+                                0px 0px;`
+        }
+        const style = `
+            background-size: ${backgroundSize};
+            background-position: ${backgroundPosition};
+
+    `;
+    progressRef.current?.setAttribute('style', style);
+
+    }, [xp]);
 
     return (<div className=" w-full h-full ">
         <div  ref={progressRef} className=" rectangulaire-progress-bar rounded-2xl flex justify-center">
@@ -32,7 +130,7 @@ const ProfileAvatar = () => {
                 rounded-xl
 
                 `}/>
-                <div className={`absolute bg-[#7743F9] text-white flex justify-center h-[1.6rem] w-[1.8rem] rounded-md -bottom-3 left-1/2 transform -translate-x-1/2 font-semibold text-xs 
+                <div className={`absolute bg-[#6d3de7] text-white flex justify-center h-[1.6rem] w-[1.8rem] rounded-md -bottom-3 left-1/2 transform -translate-x-1/2 font-semibold text-xs 
                  shadow-inner `}>
                     <p className="self-center">10</p>
                 </div>
@@ -44,16 +142,3 @@ const ProfileAvatar = () => {
 
 export default ProfileAvatar;
 
-
-/*
-background-size: 100% 10px, 10px 100%, 50% 10px, 50% 10px, 10px 100% ,100% 100%; */
-/**
- * background-position: how far from left edge , how far from top ( negative how far from bottom)
-      0px 0px,  top edge
-      100px 0px,  right edge 
-      0px 100px ,  bottom left 
-      50px 100px,  bottom right 
-      0px 0px,  left edge
-      0px 0px; gradient fill
-
- */
