7d1697050da07e408e918dd835abca6d30bf259d
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-10-15 16:13:18 +0100

    add components

--- srcs/requirements/frontend/src/app/(site)/dashboard/page.tsx
+++ srcs/requirements/frontend/src/app/(site)/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import Header from "../../components/layout/header/Header";
 import Sidebar from "../../components/layout/sidebar/Sidebar";
+import DashboardHeader from "@/app/components/dashboard/DashboardHeader";
 
 const Home = () => {
   return (
-    <div className="">
-
+    <div className=" w-full p-4">
+        <DashboardHeader/>
     </div>
   );
 };
--- srcs/requirements/frontend/src/app/(site)/user-profile/page.tsx
+++ srcs/requirements/frontend/src/app/(site)/user-profile/page.tsx
@@ -1,21 +1,23 @@
 import UserInfo from "@/app/components/userProfile/UserInfo";
 import Card from "@/app/components/userProfile/Card";
 import {MdEmojiPeople} from "react-icons/md"
+import { GiAchievement } from "react-icons/gi"
+import { RiChatHistoryFill } from "react-icons/ri"
 
 const Home = () => {
   return (
     <div className={`p-4 h-3/5 min-h-[500px]`}>
       <UserInfo />
-      <div className="w-full h-full grid grid-flow-col grid-rows-2 gap-4">
-      <Card header="achievements" icon="" >
+      <div className=" px-4 w-full h-full grid grid-flow-col grid-rows-2 gap-[1.25rem]">
+      <Card header="achievements" icon= {<GiAchievement className="text-[#6C61A4] w-6 h-6"/>} >
               <div></div>
             </Card>
 
-            <Card header="friends" icon= {<MdEmojiPeople/>}>
+            <Card header="friends" icon= {<MdEmojiPeople className="text-[#6C61A4] w-6 h-6"/>}>
             <div></div>
           </Card>
           <div className="row-span-2">
-          <Card header="game history" icon="" >
+          <Card header="game history" icon={<RiChatHistoryFill className="text-[#6C61A4] w-5 h-5" />}>
           </Card>
 
           </div>
