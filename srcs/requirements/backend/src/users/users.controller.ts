62eee33ef6eefa7a75f4c86a6a0c8799340f09b3
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-10-15 16:12:57 +0100

    add: test validation for /api/users/ route

--- srcs/requirements/backend/src/users/users.controller.ts
+++ srcs/requirements/backend/src/users/users.controller.ts
@@ -6,10 +6,12 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -21,13 +23,18 @@ export class UsersController {
   }
 
   @Get()
-  findAll() {
+  findAll(@Req() req: Request) {
+    // TODO: createa guard to check if user is logged in
+    // if (!this.usersService.isLoggedIn())
+    if (!req.user) {
+      return { message: 'You are not authorized to access this resource' };
+    }
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  findById(@Param('id') id: string) {
+    return this.usersService.findById(+id);
   }
 
   @Patch(':id')
