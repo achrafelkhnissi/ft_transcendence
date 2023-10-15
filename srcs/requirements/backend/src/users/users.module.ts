ba269572181551b433195c4648877b7f8cf7b191
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-10-15 16:12:21 +0100

    export usersService to bed used in auth module

--- srcs/requirements/backend/src/users/users.module.ts
+++ srcs/requirements/backend/src/users/users.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
   imports: [PrismaModule],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService], // TODO: Check if this is needed and why
 })
 export class UsersModule {}
