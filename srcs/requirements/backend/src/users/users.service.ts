5db45a95a5d11832f5ba17d437b8ba0d076a96e6
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-10-15 16:11:55 +0100

    add: create & findByEmail Services

--- srcs/requirements/backend/src/users/users.service.ts
+++ srcs/requirements/backend/src/users/users.service.ts
@@ -9,17 +9,26 @@ export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
   create(createUserDto: CreateUserDto) {
-    createUserDto;
-    return 'This action adds a new user';
+    return this.prisma.user.create({
+      data: {
+        ...createUserDto,
+      },
+    });
   }
 
   findAll() {
     return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
+  findById(id: number) {
     return this.prisma.user.findUnique({
       where: { id: id },
+    });
+  }
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email: email },
     });
   }
 
