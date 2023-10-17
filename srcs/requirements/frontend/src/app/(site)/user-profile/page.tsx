731ae4f74bf8f4a2829e8461081ffc8f11814d81
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-10-17 15:49:22 +0100

    fix

--- srcs/requirements/frontend/src/app/(site)/user-profile/page.tsx
+++ srcs/requirements/frontend/src/app/(site)/user-profile/page.tsx
@@ -6,9 +6,9 @@ import { RiChatHistoryFill } from "react-icons/ri"
 
 const Home = () => {
   return (
-    <div className={`p-4 h-3/5 min-h-[500px]`}>
+    <div className={`p-4  h-3/5`}>
       <UserInfo />
-      <div className=" px-4 w-full h-full grid grid-flow-col grid-rows-2 gap-[1.25rem]">
+      <div className=" px-4 w-full h-full grid grid-flow-col grid-rows-2 gap-[1.25rem] max-[880px]:grid-flow-row max-[880px]:grid-rows-3">
       <Card header="achievements" icon= {<GiAchievement className="text-[#6C61A4] w-6 h-6"/>} >
               <div></div>
             </Card>
@@ -16,7 +16,7 @@ const Home = () => {
             <Card header="friends" icon= {<MdEmojiPeople className="text-[#6C61A4] w-6 h-6"/>}>
             <div></div>
           </Card>
-          <div className="row-span-2">
+          <div className="min-[880px]:row-span-2">
           <Card header="game history" icon={<RiChatHistoryFill className="text-[#6C61A4] w-5 h-5" />}>
           </Card>
 
