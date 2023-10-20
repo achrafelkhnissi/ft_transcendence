ed6107a816029b0c8925e56b1fb4237c977d8153
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-10-20 16:11:30 +0100

    remove max-w

--- srcs/requirements/frontend/src/app/(site)/user-profile/page.tsx
+++ srcs/requirements/frontend/src/app/(site)/user-profile/page.tsx
@@ -5,7 +5,7 @@ import Friends from "@/app/components/userProfile/Friends";
 
 const Home = () => {
   return (
-    <div className={`p-4 max-w-`}>
+    <div className={`p-4`}>
       <UserInfo />
       <div className='px-4 w-full h-full grid grid-flow-col grid-rows-2 gap-[1.25rem] max-[880px]:grid-flow-row 
       max-[880px]:grid-rows-3  min-[880px]:grid-cols-2'>
