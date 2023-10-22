70010a9cacf709a5c6abcfeb728fe340f1c01fa6
Fathiya Olatokunbo Jamia <fathjami@Achrafs-MacBook-Pro.local>
2023-10-22 12:29:30 +0100

    remove min hight for the sidebar and add the meta tag

--- srcs/requirements/frontend/src/app/components/layout/sidebar/Sidebar.tsx
+++ srcs/requirements/frontend/src/app/components/layout/sidebar/Sidebar.tsx
@@ -10,7 +10,7 @@ const Sidebar = ( ) => {
     const route = useRouter();
 
     return (
-    <div  className="text-white justify-between min-h-[1000px] list-none flex flex-col bg-[#25244E] h-full w-[5rem] mt-0 bottom-0 py-4">
+    <div  className="text-white justify-between list-none flex flex-col bg-[#25244E] h-full w-[5rem] mt-0 bottom-0 py-4">
         
         <div className="  self-center ">
             <Logo />
--- srcs/requirements/frontend/src/app/layout.tsx
+++ srcs/requirements/frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Sidebar from "./components/layout/sidebar/Sidebar";
 import Header from "./components/layout/header/Header";
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
       <body className={`${inter.className} `}>
         {children}
         </body>
