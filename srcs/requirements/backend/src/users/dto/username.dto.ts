ed19e8368ec9082c47078b08fc92dee53d89fb01
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-10-23 17:28:16 +0100

    update username dto to only allow lowercase letters and dashes, and cannot start or end with a dash

--- srcs/requirements/backend/src/users/dto/username.dto.ts
+++ srcs/requirements/backend/src/users/dto/username.dto.ts
@@ -1,6 +1,9 @@
-import { IsAlpha } from 'class-validator';
+import { Matches } from 'class-validator';
 
 export class UsernameDto {
-  @IsAlpha('en-US', { message: 'Username must contain only letters' })
+  @Matches(/^[a-z]+(-[a-z]+)*$/, {
+    message:
+      'Username must contain only lowercase letters and dashes, and cannot start or end with a dash',
+  })
   username: string;
 }
