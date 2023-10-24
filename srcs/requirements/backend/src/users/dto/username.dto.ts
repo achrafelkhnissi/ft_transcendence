d3d64ac05a39db3f51d5222f68cee67c950d7df6
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-10-24 11:54:52 +0100

    add: Min length of 4 characters

--- srcs/requirements/backend/src/users/dto/username.dto.ts
+++ srcs/requirements/backend/src/users/dto/username.dto.ts
@@ -1,9 +1,12 @@
-import { Matches } from 'class-validator';
+import { Matches, MinLength } from 'class-validator';
 
 export class UsernameDto {
   @Matches(/^[a-z]+(-[a-z]+)*$/, {
     message:
       'Username must contain only lowercase letters and dashes, and cannot start or end with a dash',
+  })
+  @MinLength(4, {
+    message: 'Username must be at least 4 characters long',
   })
   username: string;
 }
