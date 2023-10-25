f2b678db7f4ac228392d470cc71004d5993fc3f5
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-10-25 19:07:02 +0100

    t

--- srcs/backend/src/notifications/notifications.controller.ts
+++ srcs/backend/src/notifications/notifications.controller.ts
@@ -6,14 +6,27 @@ import {
   Patch,
   Param,
   Delete,
+  Sse,
+  Logger,
 } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { CreateNotificationDto } from './dto/create-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
+import { Observable, interval, map } from 'rxjs';
+import { Notification, NotificationType } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private readonly notificationsService: NotificationsService) {}
+  private readonly logger = new Logger(NotificationsController.name);
+
+  constructor(
+    private readonly notificationsService: NotificationsService,
+    private readonly prismaService: PrismaService,
+  ) {}
+
+  // @Sse('sse')
+  // sendNotification(): Observable<Notification> {}
 
   @Post()
   create(@Body() createNotificationDto: CreateNotificationDto) {
--- srcs/backend/src/notifications/notifications.module.ts
+++ srcs/backend/src/notifications/notifications.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
