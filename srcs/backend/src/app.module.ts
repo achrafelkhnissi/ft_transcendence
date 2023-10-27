d453fb436e755afdd753908a207b9156285115f8
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-10-27 10:02:28 +0100

    fix: merge

--- srcs/backend/src/app.module.ts
+++ srcs/backend/src/app.module.ts
@@ -1,13 +1,39 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_INTERCEPTOR, RouterModule } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { FriendsModule } from './friends/friends.module';
+import { FriendRequestsModule } from './friend-requests/friend-requests.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, AuthModule],
+  imports: [
+    UsersModule,
+    PrismaModule,
+    AuthModule,
+    FriendsModule,
+    FriendRequestsModule,
+    RouterModule.register([
+      {
+        path: 'users',
+        module: UsersModule,
+        children: [
+          {
+            path: 'friends',
+            module: FriendsModule,
+            children: [
+              {
+                path: 'requests',
+                module: FriendRequestsModule,
+              },
+            ],
+          },
+        ],
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
