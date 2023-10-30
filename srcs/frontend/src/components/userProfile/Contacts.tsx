d9cca9339888ca19c1cee86db8493412f537fcf7
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-10-30 17:28:26 +0100

    use useEffect

--- srcs/frontend/src/components/userProfile/Contacts.tsx
+++ srcs/frontend/src/components/userProfile/Contacts.tsx
@@ -4,7 +4,7 @@ import Logo_42 from "../logos/Logo_42";
 import Link from "next/link";
 import { BiUserCheck, BiUserX, BiUserPlus } from "react-icons/bi";
 import { FriendshipStatus } from "@/app/(site)/profile/[...name]/page";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import sendFriendRequest from "@/services/sendFriendRequest";
 import removeFriend from "@/services/removeFriend";
 import cancelFriendRequest from "@/services/cancelFriendRequest";
@@ -15,102 +15,155 @@ interface ContactsProps {
   status: false | FriendshipStatus;
   url: string;
 }
+interface ContactsItemsProps {
+  label: string;
+  href: string;
+  color: string;
+  icon: React.ReactNode;
+}
 
-const handler = (event: React.MouseEvent, label: string) => {};
+const ContactsItems: { [key: string]: ContactsItemsProps } = {
+  sendMessage: {
+    label: "send a message",
+    href: "/conversation",
+    color: "#31A350",
+    icon: (
+      <TbMessageCircle2Filled className="text-white w-6 h-6 bg-[#31A350]" />
+    ),
+  },
+  sendRequest: {
+    label: "send a friend request",
+    href: "/friend-request",
+    color: "#3385FF",
+    icon: <BiUserPlus className="text-white w-6 h-6 bg-[#3385FF]" />,
+  },
+  cancelRequest: {
+    label: "cancel friend request",
+    href: "/friend-request",
+    color: "#3385FF",
+    icon: <BiUserX className="text-white w-6 h-6 bg-[#3385FF]" />,
+  },
+  acceptRequest: {
+    label: "accept friend request",
+    href: "/friend-request",
+    color: "#3385FF",
+    icon: <BiUserCheck className="text-white w-6 h-6 bg-[#3385FF]" />,
+  },
+  intra: {
+    label: "intra profile ",
+    href: "/profile-intra",
+    color: "#6257FE",
+    icon: <Logo_42 color="white" width="1.7rem" height="1.5rem" />,
+  },
+};
+
+interface statePorps {
+  stats: false | FriendshipStatus;
+  isClicked: "send" | "cancel" | "friend" | "";
+}
 
 const Contacts: React.FC<ContactsProps> = ({ username, me, status, url }) => {
-  interface ContactsProps {
-    label: string;
-    href: string;
-    color: string;
-    icon: React.ReactNode;
-  }
+  const [state, setState] = useState<statePorps>({
+    stats: status,
+    isClicked: "",
+  });
 
-  const ContactsItems: ContactsProps[] = [
-    {
-      label: "send message",
-      href: "/conversation",
-      color: "#31A350",
-      icon: (
-        <TbMessageCircle2Filled className="text-white w-6 h-6 bg-[#31A350]" />
-      ),
-    },
-    {
-      label: "add friend",
-      href: "/friend-request",
-      color: "#3385FF",
-      icon: <BiUserPlus className="text-white w-6 h-6 bg-[#3385FF]" />,
-    },
-    {
-      label: "cancel request",
-      href: "/friend-request",
-      color: "#3385FF",
-      icon: <BiUserX className="text-white w-6 h-6 bg-[#3385FF]" />,
-    },
-    {
-      label: "friend",
-      href: "/friend-request",
-      color: "#3385FF",
-      icon: <BiUserCheck className="text-white w-6 h-6 bg-[#3385FF]" />,
-    },
-    // {
-    //   label: "intra profile",
-    //   href: "/profile-intra",
-    //   color: "#6257FE",
-    //   icon: <Logo_42 color="white" width="1.7rem" height="1.5rem" />,
-    // },
-  ];
+  useEffect(() => {
+    if (state.isClicked == "send") {
+      sendFriendRequest(username).then((res) => {
+        setState({
+          isClicked: "",
+          stats: res.request.friendshipStatus,
+        });
+      });
+    }
+  }, [state, username]);
 
   return (
     <div className="flex justify-center gap-4 px-6">
-      {ContactsItems.map((item, index) => {
-        if (
-          !me &&
-          (item.label == "send message" ||
-            (item.label == "add friend" &&
-              status === false) || 
-            (item.label == "cancel request" &&
-              status === FriendshipStatus.PENDING) ||
-            (item.label == "friend" && status == FriendshipStatus.ACCEPTED))
-        )
-          return (
-            <button
-              key={index}
-              onClick={(e) => {
-                if (item.label == "add friend") {
-                  e.preventDefault();
-                  sendFriendRequest(username);
-                } else if (item.label == "cancel request") {
-                  e.preventDefault();
-                  cancelFriendRequest(username);
-                } else if (item.label == "friend") {
-                  e.preventDefault();
-                  removeFriend(username);
-                }
-              }}
-              className={`
-              rounded-xl
-              bg-[${item.color}]
-              p-2
-              `}
-            >
-              {item.icon}
-            </button>
-          );
-      })}
+      {!me && (state.stats == false && (
+        <button
+          onClick={() => {
+            setState({
+              stats: status,
+              isClicked: "send",
+            });
+          }}
+          className={`
+          rounded-xl
+          bg-[${ContactsItems.sendRequest.color}]
+          p-2
+          `}
+        >
+          {ContactsItems.sendRequest.icon}
+        </button>
+      )
+      )
+      }
+
       <Link
         target="_blank"
         href={url}
         className={`
-                                rounded-xl
-                                bg-[#6257FE]
-                                p-2
-                                `}
+                        rounded-xl
+                        bg-[#6257FE]
+                        p-2
+                        `}
       >
-        <Logo_42 color="white" width="1.7rem" height="1.5rem" />{" "}
+        {ContactsItems.intra.icon}
       </Link>
     </div>
   );
 };
 
 export default Contacts;
+
+//   return (
+//     <div className="flex justify-center gap-4 px-6">
+//       {ContactsItems.map((item, index) => {
+//         if (
+//           !me &&
+//           (item.label == "send message" ||
+//             (item.label == "add friend" && status === false) ||
+//             (item.label == "cancel request" &&
+//               status === FriendshipStatus.PENDING) ||
+//             (item.label == "friend" && status == FriendshipStatus.ACCEPTED))
+//         )
+//           return (
+//             <button
+//               key={index}
+//               onClick={(e) => {
+//                 if (item.label == "add friend") {
+//                   e.preventDefault();
+//                   sendFriendRequest(username);
+//                 } else if (item.label == "cancel request") {
+//                   e.preventDefault();
+//                   cancelFriendRequest(username);
+//                 } else if (item.label == "friend") {
+//                   e.preventDefault();
+//                   removeFriend(username);
+//                 }
+//               }}
+//               className={`
+//               rounded-xl
+//               bg-[${item.color}]
+//               p-2
+//               `}
+//             >
+//               {item.icon}
+//             </button>
+//           );
+//       })}
+//       <Link
+//         target="_blank"
+//         href={url}
+//         className={`
+//                                 rounded-xl
+//                                 bg-[#6257FE]
+//                                 p-2
+//                                 `}
+//       >
+//         <Logo_42 color="white" width="1.7rem" height="1.5rem" />{" "}
+//       </Link>
+//     </div>
+//   );
