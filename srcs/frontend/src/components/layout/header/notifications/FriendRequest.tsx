dc58f1db5fbad73de53ce0b88072280dedfcc2cc
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-11-11 19:52:35 +0100

    update

--- srcs/frontend/src/components/layout/header/notifications/FriendRequest.tsx
+++ srcs/frontend/src/components/layout/header/notifications/FriendRequest.tsx
@@ -1,12 +1,15 @@
-import {NotificationsType} from "./Notifications"
+import { NotificationsType } from "./Notifications";
 
 const FriendRequest: React.FC<NotificationsType> = (notif) => {
+  return (
+    <div className="bordder  text-white w-full h-20 text-sm p-2 ">
+      <p className="text-center">{notif.content}</p>
+      <div className="flex justify-center gap-4 py-2">
+        <button className="px-4 py-[0.4rem] bg-[#6257FE] rounded-md">accept</button>
+        <button className="px-4 py-[0.4rem] bg-red-600">decline</button>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="bordder bg-[#6257FE]/80 text-white w-full h-16 text-sm p-2 text-center">
-            {notif.content}
-        </div>
-    )
-}
-
-export default FriendRequest
+export default FriendRequest;
