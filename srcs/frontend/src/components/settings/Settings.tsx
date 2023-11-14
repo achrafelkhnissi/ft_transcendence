085005ce5a115340ecabe3d58974b5f3b5870357
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-11-15 00:11:54 +0100

    implement settings ui and functionalities

--- srcs/frontend/src/components/settings/Settings.tsx
+++ srcs/frontend/src/components/settings/Settings.tsx
@@ -1,48 +1,126 @@
 "use client";
-import { useState } from "react";
+import Image from "next/image";
+import { ChangeEvent, useState, useEffect } from "react";
+import getCurrentUser from "@/services/getCurrentUser";
 
-// const Settings = () => {
+export interface Data {
+  username: string;
+  avatar: string;
+  twoFA: boolean;
+}
 
-//   return (
-//     <div className=" w-full h-full rounded-[2rem] z-10 text-white  ">
-//         <form>
-//             <input
-//             type="file"
-//             className="rounded-full ">
-//             </input>
-//         </form>
-//     </div>
-//   );
-// };
+const defaultData: Data = {
+  username: "",
+  avatar: "",
+  twoFA: false,
+};
 
 const Settings = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [data, setData] = useState<Data>(defaultData);
 
-  const handleFileChange = (e: any) => {
-    const file = e.target.files[0];
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
 
     if (file) {
-      setSelectedFile(file);
+      setData((prev) => {
+        return {
+          ...prev,
+          avatar: URL.createObjectURL(file),
+        };
+      });
     }
   };
 
+  const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData((prev) => ({
+      ...prev,
+      username: e.target.value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    console.log(data.username);
+  }
+
+    const [isSwitchOn, setSwitchOn] = useState(false);
+  
+    const handleToggleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setSwitchOn(e.target.checked);
+    };
+
+    
+  useEffect(() => {
+    getCurrentUser().then((res) => {
+      const ret: Data = res;
+      setData(ret);
+    });
+  }, []);
+
   return (
-    <div className="mt-4">
-      <div className="mt-1 flex items-center">
-        <label
-          htmlFor="fileInput"
-          className="cursor-pointer border rounded-full w-4 h-4 text-white flex justify-center"
-        >
-          <p className=" self-center">+</p>
-        </label>
-        <input
-          type="file"
-          id="fileInput"
-          className="sr-only"
-          onChange={handleFileChange}
+    <form className=" flex flex-col justify-center py-8 gap-20">
+      <div className="relative inline-block m-auto">
+        <Image
+          src={data.avatar}
+          alt="image"
+          width={100}
+          height={100}
+          className="w-28 h-28 rounded-full border-4 border-purple-400/50 object-fill"
         />
+        <div className="flex items-center absolute bottom-2 right-2">
+          <label
+            htmlFor="fileInput"
+            className="cursor-pointer border rounded-full w-[1.3rem] h-[1.3rem] text-white flex justify-center bg-white"
+          >
+            <p className=" self-center text-lg -mt-[0.1rem] text-purple-500 font-bold ">
+              +
+            </p>
+          </label>
+          <input
+            type="file"
+            id="avatar"
+            className="sr-only"
+            onChange={handleFileChange}
+          />
+        </div>
       </div>
+        <input
+          type="text"
+          id="username"
+          placeholder={data.username}
+          onChange={handleUsernameChange}
+          className="w-3/5 h-12 rounded-xl border-2 border-purple-400/50 bg-white/5 self-center outline-none px-4
+       text-white/60 text-md font-normal placeholder:opacity-40
+      "
+        />
+       <div className="flex items-center">
+      <label htmlFor="toggleSwitch" className="mr-2">Toggle Switch</label>
+      <div className="relative">
+        <input
+          type="checkbox"
+          id="toggleSwitch"
+          checked={isSwitchOn}
+          onChange={handleToggleChange}
+          className="sr-only"
+        />
+        <div
+          className={`w-10 h-5 bg-gray-300 rounded-full transition-transform duration-300 ease-in-out ${
+            isSwitchOn ? 'bg-green-500' : ''
+          }`}
+        >
+          <div
+            className={`absolute w-5 h-5 bg-white rounded-full transform transition-transform duration-300 ease-in-out ${
+              isSwitchOn ? 'translate-x-full' : ''
+            }`}
+          ></div>
+        </div>
+      </div>
+      <span className="ml-2">{isSwitchOn ? 'On' : 'Off'}</span>
     </div>
+      <button className=" text-white/80 m-auto px-4 py-1 rounded-lg absolute bottom-20 right-10  bg-purple-400/50 "
+      onClick={handleSubmit}>
+          save
+      </button>
+    </form>
   );
 };
 export default Settings;
