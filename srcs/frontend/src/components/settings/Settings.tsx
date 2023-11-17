28acc7c828dbbb6a68525b97803c4cfe082ef71b
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2023-11-17 17:36:49 +0100

    make inputs read-only until edit state is true

--- srcs/frontend/src/components/settings/Settings.tsx
+++ srcs/frontend/src/components/settings/Settings.tsx
@@ -2,21 +2,56 @@
 import Image from "next/image";
 import { ChangeEvent, useState, useEffect } from "react";
 import getCurrentUser from "@/services/getCurrentUser";
+import { MdModeEdit } from "react-icons/md";
+import getAllUsers from "@/services/getAllUsers";
 
 export interface Data {
   username: string;
   avatar: string;
   twoFA: boolean;
+  phoneNumber: string | null;
+}
+
+interface Users {
+  username: string;
 }
 
 const defaultData: Data = {
   username: "",
   avatar: "",
   twoFA: false,
+  phoneNumber: null,
 };
 
 const Settings = () => {
   const [data, setData] = useState<Data>(defaultData);
+  const [editUserName, setEditUserName] = useState<boolean>(false);
+  const [editPhoneNumber, setEditPhoneNumber] = useState<boolean>(false);
+  const [isSwitchOn, setSwitchOn] = useState(false);
+  const [Users, setUsers] = useState<Users[]>();
+  const [unique, setUnique] = useState<boolean>(true);
+
+  useEffect(() => {
+    getCurrentUser().then((res) => {
+      const ret: Data = res;
+      setData(ret);
+    });
+
+    // getAllUsers().then((res) => {
+    setUsers([
+      { username: "fathjami" },
+      { username: "ael-khni" },
+      { username: "zsarir" },
+    ]);
+    // });
+  }, []);
+
+  const handleToggleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    //check if there is a number provided;
+    if (data.phoneNumber && data.phoneNumber !== "")
+      setSwitchOn(true);
+    // send verification code to enabe 2FA
+  };
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -32,9 +67,20 @@ const Settings = () => {
   };
 
   const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.value;
+    if (name === "" || Users?.every((obj) => !obj.username.includes(name))) {
+      setUnique(true);
+      setData((prev) => ({
+        ...prev,
+        username: name,
+      }));
+    } else setUnique(false);
+  };
+
+  const handlePhoneNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
     setData((prev) => ({
       ...prev,
-      username: e.target.value,
+      phoneNumber: e.target.value,
     }));
   };
 
@@ -42,21 +88,17 @@ const Settings = () => {
     console.log(data.username);
   };
 
-  const [isSwitchOn, setSwitchOn] = useState(false);
-
-  const handleToggleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSwitchOn(e.target.checked);
+  const handleEditPhoneNumber = () => {
+    if (!data.phoneNumber) setEditPhoneNumber(true);
+    else {
+      // send a verificatin code to the prev number
+      // when verified allow the user to modify the number
+    }
   };
-
-  useEffect(() => {
-    getCurrentUser().then((res) => {
-      const ret: Data = res;
-      setData(ret);
-    });
-  }, []);
 
   return (
     <form className=" flex flex-col justify-center py-8 gap-10">
+      {/* edit image */}
       <div className="relative inline-block m-auto">
         <Image
           src={data.avatar}
@@ -70,9 +112,7 @@ const Settings = () => {
             htmlFor="avatar"
             className="cursor-pointer border rounded-full w-[1.3rem] h-[1.3rem] text-white flex justify-center bg-white"
           >
-            <p className=" self-center text-lg -mt-[0.1rem] text-purple-500 font-bold ">
-              +
-            </p>
+            <MdModeEdit className=" self-center text-lg -mt-[0.1rem] text-purple-500 font-bold w-4 h-4" />
           </label>
           <input
             type="file"
@@ -82,23 +122,60 @@ const Settings = () => {
           />
         </div>
       </div>
-      <input
-        type="text"
-        id="username"
-        placeholder={data.username}
-        onChange={handleUsernameChange}
-        className="w-3/5 h-12 rounded-xl border-2 border-purple-400/50 bg-white/5 self-center outline-none px-4
+
+      {/* edit username */}
+      <div className="flex flex-col gap-1 ">
+        <div className="flex justify-center gap-2">
+          <input
+            type="text"
+            id="username"
+            readOnly={!editUserName}
+            placeholder={data.username !== "" ? data.username : "username"}
+            onChange={handleUsernameChange}
+            className="w-3/5 h-12 rounded-xl border-2 border-purple-400/50 bg-white/5 outline-none px-4
        text-white/60 text-md font-normal placeholder:opacity-40
       "
-      />
+          />
+          <label
+            htmlFor="username"
+            className="cursor-pointer border self-center rounded-full w-[1.3rem] h-[1.3rem] text-white  bg-white flex justify-center"
+          >
+            <MdModeEdit
+              className="text-purple-500 font-bold self-center w-4 h-4"
+              onClick={() => setEditUserName(true)}
+            />
+          </label>
+        </div>
+        {!unique && (
+          <p className="text-red-500 text-xs mx-auto">
+            Username already exists. Please choose another one.
+          </p>
+        )}
+      </div>
+
+      {/* edit phone number */}
+      <div className={`flex justify-center gap-2 `}>
         <input
           type="text"
           id="phone-number"
-          placeholder={"XXXXXXXX41"}
-          // onChange={handleUsernameChange}
+          readOnly={!editPhoneNumber}
+          placeholder={data.phoneNumber ? data.phoneNumber : "+212 XXX XXX"}
+          onChange={handlePhoneNumberChange}
           className={`w-3/5 h-12 rounded-xl border-2 border-purple-400/50 bg-white/5 self-center outline-none px-4
-       text-white/60 text-md font-normal placeholder:opacity-40 ${!isSwitchOn && "hidden"}`}
+       text-white/60 text-md font-normal placeholder:opacity-40 `}
         />
+        <label
+          htmlFor="phone-number"
+          className="cursor-pointer border self-center rounded-full w-[1.3rem] h-[1.3rem] text-white  bg-white flex justify-center"
+        >
+          <MdModeEdit
+            className="text-purple-500 font-bold self-center w-4 h-4"
+            onClick={handleEditPhoneNumber}
+          />
+        </label>
+      </div>
+
+      {/* 2FA  */}
       <div className="flex items-center text-white  m-auto">
         <label
           htmlFor="toggleSwitch"
@@ -112,6 +189,8 @@ const Settings = () => {
             checked={isSwitchOn}
             onChange={handleToggleChange}
             className="sr-only"
+            readOnly={isSwitchOn}
+            //make it editibale if the phoneNumber exitsts
           />
           <div
             className={`absolute w-5 h-5  rounded-full transform transition-transform duration-300 ease-in-out cursor-none ${
@@ -121,6 +200,8 @@ const Settings = () => {
         </label>
         <span className="ml-2 text-white/80">Two Factor Authentication </span>
       </div>
+
+      {/* Submit form */}
       <button
         className=" text-white/80 m-auto px-4 py-1 rounded-lg absolute bottom-20 right-10  bg-purple-400/50 "
         onClick={handleSubmit}
