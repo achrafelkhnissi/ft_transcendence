48fed7ad9363a98a028346b287aedb1fd8accaea
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-11-18 13:05:46 +0100

    add: import prisma

--- srcs/backend/src/sms/sms.module.ts
+++ srcs/backend/src/sms/sms.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SmsService } from './sms.service';
 import { SmsController } from './sms.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [SmsController],
   providers: [SmsService],
 })
