45c26a022aafddb8f9733e4d1dc39c081bf9ea92
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-11-18 12:23:53 +0100

    add: sms

---
+++ srcs/backend/src/sms/sms.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { SmsService } from './sms.service';
+
+@Controller('sms')
+export class SmsController {
+  constructor(private readonly smsService: SmsService) {}
+}
