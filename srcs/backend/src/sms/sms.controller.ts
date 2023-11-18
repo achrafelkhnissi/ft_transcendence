edeeab4b69006362c9dbcde818ccb12220c06850
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-11-18 13:06:24 +0100

    add: verity & confirm controllers and services

--- srcs/backend/src/sms/sms.controller.ts
+++ srcs/backend/src/sms/sms.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, Post } from '@nestjs/common';
 import { SmsService } from './sms.service';
+import { User } from 'src/decorators/user.decorator';
+import { UserType } from 'src/interfaces/user.interface';
 
 @Controller('sms')
 export class SmsController {
   constructor(private readonly smsService: SmsService) {}
+
+  @Post('verify')
+  verify(@User() user: UserType) {
+    if (user.isPhoneNumberVerified) {
+      return new BadRequestException('Phone number already verified');
+    }
+
+    return this.smsService.initiatePhoneNumberVerification(user.phoneNumber);
+  }
+
+  @Post('confirm')
+  confirm(@User() user: UserType, code: string) {
+    if (user.isPhoneNumberVerified) {
+      return new BadRequestException('Phone number already verified');
+    }
+
+    return this.smsService.confirmPhoneNumberVerification(
+      user.id,
+      user.phoneNumber,
+      code,
+    );
+  }
 }
--- srcs/backend/src/sms/sms.service.ts
+++ srcs/backend/src/sms/sms.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Twilio } from 'twilio';
 
 @Injectable()
-export class SmsService {}
+export class SmsService {
+  private twilioClient: Twilio;
+  constructor(private readonly prismaService: PrismaService) {
+    this.twilioClient = new Twilio(
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_AUTH_TOKEN,
+    );
+  }
+
+  async initiatePhoneNumberVerification(phoneNumber: string) {
+    return this.twilioClient.verify
+      .services(process.env.TWILIO_VERIFY_SERVICE_SID)
+      .verifications.create({
+        to: phoneNumber,
+        channel: 'sms',
+      });
+  }
+
+  async confirmPhoneNumberVerification(
+    userId: number,
+    phoneNumber: string,
+    code: string,
+  ) {
+    const result = await this.twilioClient.verify
+      .services(process.env.TWILIO_VERIFY_SERVICE_SID)
+      .verificationChecks.create({
+        to: phoneNumber,
+        code,
+      });
+
+    if (!result.valid || result.status !== 'approved') {
+      throw new BadRequestException('Invalid verification code');
+    }
+
+    return await this.updatePhoneNumberVerificationStatus(userId);
+  }
+
+  async updatePhoneNumberVerificationStatus(userId: number) {
+    return await this.prismaService.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        isPhoneNumberVerified: true,
+      },
+    });
+  }
+}
