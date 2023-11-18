c53226719b19b13950376299cfb3be85e2f08b29
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-11-18 13:55:16 +0100

    rm: Get

--- srcs/backend/src/sms/sms.controller.ts
+++ srcs/backend/src/sms/sms.controller.ts
@@ -1,10 +1,4 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Get,
-  Post,
-} from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { SmsService } from './sms.service';
 import { User } from 'src/decorators/user.decorator';
 import { UserType } from 'src/interfaces/user.interface';
