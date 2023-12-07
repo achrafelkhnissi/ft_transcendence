ef3d6105cc5ae2958e0b1e789c8bd66e365dcf0e
Fathiya Olatokunbo Jamia <fathjami@Achrafs-MacBook-Pro.local>
2023-12-07 19:25:33 +0100

    add preview switch

--- srcs/frontend/src/components/messages/Preview.tsx
+++ srcs/frontend/src/components/messages/Preview.tsx
@@ -1,7 +1,12 @@
+import PreviewSwitch from "./PreviewSwitch"
+
 const Preview = () => {
     return (<div 
             className="w-2/5  bg-[#25244E] rounded-[3rem] 
-            shadow-[0_20px_40px_15px_rgba(0,0,0,0.2)]">
+            shadow-[0_20px_40px_15px_rgba(0,0,0,0.2)] p-2" >
+                <div className="w-full flex flex-col justify-center pt-2">
+                    <PreviewSwitch/>
+                </div>
         </div>)
 }
 
