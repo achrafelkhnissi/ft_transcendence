6bd1ff59ba3248be0a39a7f0be26d2a5370734dc
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-12-14 15:52:20 +0100

    add: dto for creating a msg

--- srcs/backend/src/message/dto/create-message.dto.ts
+++ srcs/backend/src/message/dto/create-message.dto.ts
@@ -1,1 +1,20 @@
-export class CreateMessageDto {}
+// create-message.dto.ts
+import { IsNotEmpty, IsInt, IsString } from 'class-validator';
+
+export class CreateMessageDto {
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  chatId: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  senderId: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  receiverId: number;
+}
