b2f3757e3c68a0de0f70ca48c4e19e4e78438bae
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-12-14 15:53:34 +0100

    add: import prisma

--- srcs/backend/src/message/message.module.ts
+++ srcs/backend/src/message/message.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [MessageController],
   providers: [MessageService],
 })
