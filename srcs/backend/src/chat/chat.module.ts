7ea405db706acb93ec991ffd12d489a3267c258b
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-12-14 16:54:53 +0100

    add: basic needed endpoints for chat

--- srcs/backend/src/chat/chat.module.ts
+++ srcs/backend/src/chat/chat.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ChatController],
   providers: [ChatService],
 })
--- srcs/backend/src/chat/chat.service.ts
+++ srcs/backend/src/chat/chat.service.ts
@@ -1,24 +1,147 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { UpdateChatDto } from './dto/update-chat.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ChatService {
-  create(createChatDto: CreateChatDto) {}
+  private readonly logger = new Logger(ChatService.name);
+
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createChatDto: CreateChatDto) {
+    this.logger.log(`Creating chat with data ${JSON.stringify(createChatDto)}`);
+    return this.prismaService.chat.create({
+      data: createChatDto,
+    });
+  }
 
   findAll() {
-    return `This action returns all chat`;
+    this.logger.log('Finding all chats');
+    return this.prismaService.chat.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} chat`;
+    this.logger.log(`Finding chat with id ${id}`);
+    return this.prismaService.chat.findUnique({
+      where: {
+        id,
+      },
+    });
   }
 
   update(id: number, updateChatDto: UpdateChatDto) {
-    return `This action updates a #${id} chat`;
+    this.logger.log(`Updating chat with id ${id}`);
+
+    return this.prismaService.chat.update({
+      where: {
+        id,
+      },
+      data: updateChatDto,
+    });
   }
 
+  // TODO: Check if the logged in user has permission to remove a chat
   remove(id: number) {
-    return `This action removes a #${id} chat`;
+    this.logger.log(`Removing chat with id ${id}`);
+    return this.prismaService.chat.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  // TODO: maybe use message service instead?
+  findMessages(id: number) {
+    this.logger.log(`Finding messages for chat with id ${id}`);
+    return this.prismaService.chat
+      .findUnique({
+        where: {
+          id,
+        },
+      })
+      .messages();
+  }
+
+  findParticipants(id: number) {
+    this.logger.log(`Finding participants for chat with id ${id}`);
+    return this.prismaService.chat
+      .findUnique({
+        where: {
+          id,
+        },
+      })
+      .users();
+  }
+
+  findAdmins(id: number) {
+    this.logger.log(`Finding admins for chat with id ${id}`);
+    return this.prismaService.chat
+      .findUnique({
+        where: {
+          id,
+        },
+      })
+      .admins();
+  }
+
+  findOwner(id: number) {
+    this.logger.log(`Finding owner for chat with id ${id}`);
+    return this.prismaService.chat
+      .findUnique({
+        where: {
+          id,
+        },
+      })
+      .owner();
+  }
+
+  addAdmin(id: number, userId: number) {
+    this.logger.log(`Adding admin with id ${userId} to chat with id ${id}`);
+    return this.prismaService.chat.update({
+      where: {
+        id,
+      },
+      data: {
+        admins: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+  }
+
+  // TODO: Check if the logged in user has permission to remove a user from a chat
+  removeUser(id: number, userId: number) {
+    this.logger.log(`Removing user with id ${userId} from chat with id ${id}`);
+    return this.prismaService.chat.update({
+      where: {
+        id,
+      },
+      data: {
+        users: {
+          disconnect: {
+            id: userId,
+          },
+        },
+      },
+    });
+  }
+
+  removeAdmin(id: number, userId: number) {
+    this.logger.log(`Removing admin with id ${userId} from chat with id ${id}`);
+    return this.prismaService.chat.update({
+      where: {
+        id,
+      },
+      data: {
+        admins: {
+          disconnect: {
+            id: userId,
+          },
+        },
+      },
+    });
   }
 }
