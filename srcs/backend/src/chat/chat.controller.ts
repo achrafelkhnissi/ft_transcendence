1692bc319cc8aa637da16f16fb47494cfe8ff618
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-12-14 15:07:53 +0100

    add: init chat module

---
+++ srcs/backend/src/chat/chat.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { UpdateChatDto } from './dto/update-chat.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
+
+@UseGuards(AuthGuard)
+@Controller()
+export class ChatController {
+  constructor(private readonly chatService: ChatService) {}
+
+  @Post()
+  create(@Body() createChatDto: CreateChatDto) {
+    return this.chatService.create(createChatDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.chatService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.chatService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateChatDto: UpdateChatDto) {
+    return this.chatService.update(+id, updateChatDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.chatService.remove(+id);
+  }
+}
