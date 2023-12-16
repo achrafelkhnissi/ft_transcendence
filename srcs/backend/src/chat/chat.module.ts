6ebf62c1fd69444cc29c0a39f4b6b79f3e7f5a01
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-12-16 15:06:53 +0100

    Add: ChatGateway as a provider to ChatModule

--- srcs/backend/src/chat/chat.module.ts
+++ srcs/backend/src/chat/chat.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, ChatGateway],
 })
 export class ChatModule {}
