e031b0e8ffd2f83fc02b0658248bfa1d81d15a93
Fathiya Olatokunbo Jamia <fathjami@Achrafs-MacBook-Pro.local>
2023-12-23 11:16:07 +0100

    update api

--- srcs/frontend/src/services/getCurrentUser.tsx
+++ srcs/frontend/src/services/getCurrentUser.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 async function getCurrentUser() {
 
-  const { data } = await axios.get("http://localhost:3000/api/auth/whoami", {withCredentials: true} );
+  const { data } = await axios.get("http://localhost:3000/api/users/me", {withCredentials: true} );
 
   return data;
 }
