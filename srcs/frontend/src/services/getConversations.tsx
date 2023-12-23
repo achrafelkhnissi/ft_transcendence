0f4fe8dca4537fb3a9ce7e1f16417415fd705d34
Fathiya Olatokunbo Jamia <fathjami@Achrafs-MacBook-Pro.local>
2023-12-23 15:16:47 +0100

    add service

---
+++ srcs/frontend/src/services/getConversations.tsx
@@ -0,0 +1,10 @@
+import axios  from "axios"
+
+async function getConversations() {
+    const { data } = await axios.get(`http://localhost:3000/api/users/chat`,
+    {withCredentials: true} );
+ 
+   return data;
+}
+
+export default getConversations;
