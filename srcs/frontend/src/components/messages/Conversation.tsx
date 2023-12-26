d9079051e8eb550777266de43311ef715bfdf67e
Fathiya Olatokunbo Jamia <fathjami@Achrafs-MacBook-Pro.local>
2023-12-26 08:37:28 +0100

    move files

--- srcs/frontend/src/components/messages/Conversation.tsx
+++
@@ -1,9 +0,0 @@
-const Conversation = () =>{
-    return (
-    <div
-        className="w-4/6 bg-[#25244E] rounded-[3rem] max-[900px]:w-full
-        shadow-[0_10px_20px_15px_rgba(0,0,0,0.2)]">
-    </div>)
-}
-
-export default Conversation;
