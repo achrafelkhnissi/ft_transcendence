e3a61b77de6ec6974603d73ec28cf978e52c8574
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2024-01-22 22:30:47 +0100

    Refactor CreateChatDto class in chat module

--- srcs/backend/src/chat/dto/create-chat.dto.ts
+++ srcs/backend/src/chat/dto/create-chat.dto.ts
@@ -1,18 +1,8 @@
-import { $Enums, User } from '@prisma/client';
-import {
-  IsEnum,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-  IsString,
-} from 'class-validator';
+import { $Enums } from '@prisma/client';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
+// TODO: validate name & image & password
 export class CreateChatDto {
-  @IsNumber()
-  @IsOptional()
-  id?: number;
-
   @IsEnum($Enums.ConversationType)
   @IsNotEmpty()
   type: $Enums.ConversationType;
@@ -28,13 +18,4 @@ export class CreateChatDto {
   @IsOptional()
   @IsNotEmpty()
   password?: string; // TODO: How to make the password required if the chat is protected?
-
-  @IsNotEmpty()
-  @IsNumber()
-  @IsPositive()
-  ownerId?: number;
-
-  admins: User[]; // TODO: Check if you need/can use UserDto here
-
-  participants: User[];
 }
