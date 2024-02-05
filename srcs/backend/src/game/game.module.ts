d44aaefb51184816975c4e23f4ccb3b4f18deceb
zsarir23 <zainebsarir@gmail.com>
2024-02-05 16:58:54 +0100

    file problem

--- srcs/backend/src/game/game.module.ts
+++ srcs/backend/src/game/game.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GameService } from './game.service';
 import { GameController } from './game.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [GameController],
   providers: [GameService],
+  exports : [GameService],
 })
 export class GameModule {}
