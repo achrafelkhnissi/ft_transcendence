889f88fabfe4bfa48200a9e773a7bf5e3f315a0e
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2024-02-05 15:31:34 +0100

    Refactor game controller

--- srcs/backend/src/game/game.controller.ts
+++ srcs/backend/src/game/game.controller.ts
@@ -1,34 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { GameService } from './game.service';
-import { CreateGameDto } from './dto/create-game.dto';
-import { UpdateGameDto } from './dto/update-game.dto';
+import { Controller } from '@nestjs/common';
 
 @Controller('game')
-export class GameController {
-  constructor(private readonly gameService: GameService) {}
-
-  @Post()
-  create(@Body() createGameDto: CreateGameDto) {
-    return this.gameService.create(createGameDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.gameService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.gameService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGameDto: UpdateGameDto) {
-    return this.gameService.update(+id, updateGameDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.gameService.remove(+id);
-  }
-}
+export class GameController {}
