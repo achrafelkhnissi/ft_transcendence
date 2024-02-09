11eb05c7bfd5b32941639cbeaccfd5be0116e6b0
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2024-02-09 07:09:09 +0100

    Add Swagger annotations to getAuthors endpoint

--- srcs/backend/src/app.controller.ts
+++ srcs/backend/src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOkResponse({ description: 'Return authors' })
+  @ApiOperation({ summary: 'Get authors' })
   @Get('authors')
   getAuthors(): object {
     return this.appService.getAuthors();
