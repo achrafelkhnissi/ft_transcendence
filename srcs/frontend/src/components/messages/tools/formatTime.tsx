0557f1dd3d64b698897b88961fff68b2013e726d
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2024-02-12 17:27:02 +0100

    fix date

--- srcs/frontend/src/components/messages/tools/formatTime.tsx
+++ srcs/frontend/src/components/messages/tools/formatTime.tsx
@@ -2,19 +2,19 @@ function formatChatTimestamp(isoString: string): string {
   const timestamp = new Date(isoString);
   const now = new Date();
 
-  // Calculate the difference in days
+  // Calculate the difference in hours
   const diffTime = Math.abs(now.getTime() - timestamp.getTime());
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const diffHours = Math.ceil(diffTime / (1000 * 60 * 60));
 
-  if (diffDays === 1 && now.getDate() - timestamp.getDate() === 1) {
-    return 'yesterday';
-  } else if (diffDays < 1) {
+  if (diffHours < 24) {
     // Format as '03:33 PM'
     return timestamp.toLocaleTimeString('en-US', {
       hour: '2-digit',
       minute: '2-digit',
       hour12: true,
     });
+  } else if (diffHours >= 24 && diffHours < 48) {
+    return 'yesterday';
   } else {
     // Format as '23/12/23'
     return timestamp.toLocaleDateString('en-US', {
