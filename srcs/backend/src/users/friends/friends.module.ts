bc836d36c1677cac233e0a0dc6dbe4925dd50035
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2024-02-17 16:00:40 +0100

    Add Gateway module to Friends module

--- srcs/backend/src/users/friends/friends.module.ts
+++ srcs/backend/src/users/friends/friends.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FriendsService } from './friends.service';
 import { FriendsController } from './friends.controller';
 import { FriendRequestsModule } from './requests/requests.module';
+import { GatewayModule } from 'src/gateway/gateway.module';
 
 @Module({
-  imports: [FriendRequestsModule],
+  imports: [FriendRequestsModule, GatewayModule],
   controllers: [FriendsController],
   providers: [FriendsService],
   exports: [FriendsService],
