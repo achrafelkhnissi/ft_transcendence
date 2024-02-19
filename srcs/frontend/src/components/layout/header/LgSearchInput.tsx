7005cd8157801196a98ab886ea4ee10bd48aaa2a
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2024-02-19 16:59:04 +0100

    fix serach

--- srcs/frontend/src/components/layout/header/LgSearchInput.tsx
+++ srcs/frontend/src/components/layout/header/LgSearchInput.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 
 import { LuSearch } from 'react-icons/lu';
@@ -5,18 +6,18 @@ import { CiSearch } from 'react-icons/ci';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import getAllUsers from '@/services/getAllUsers';
+import { User } from '@/components/messages/data';
 
 const SearchInput = () => {
   const router = useRouter();
   const [searchQuery, setSearchQuery] = useState('');
   const [input, setInput] = useState('');
-  const [searchResultsUsers, setSearchResultsUsers] = useState([]);
-  const [searchResultsRooms, setSearchResultsRooms] = useState([]);
+  const [searchResultsUsers, setSearchResultsUsers] = useState<User[]>([]);
   const [showSearchResults, setShowSearchResults] = useState(false);
-  const [allUsers, setAllUsers] = useState([]);
+  const [allUsers, setAllUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    getAllUsers().then((data) => {
+    getAllUsers().then((data: User[]) => {
       if (data) {
         setAllUsers(data);
       }
@@ -30,6 +31,14 @@ const SearchInput = () => {
     console.log('searching for', searchQuery);
   };
 
+  const searchUsers = (query: string) => {
+    setSearchResultsUsers(
+      allUsers.filter((user) =>
+        user.username.toLowerCase().includes(query.toLowerCase()),
+      ),
+    );
+  };
+
   return (
     <div className="bg-[#424283] border-[0.09rem] border-white/50 rounded-xl relative lg:w-[400px] w-[250px] py-[0.1rem z-10 px-2">
       <input
@@ -38,14 +47,14 @@ const SearchInput = () => {
         className="w-full py-[0.4rem] px-2 bg-transparent outline-none  text-sm text-white/80  font-light placeholder:font-extralight placeholder:italic placeholder:opacity-80 "
         placeholder="search something..."
         onChange={(event) => {
-          setSearchQuery(event.target.value);
           setInput(event.target.value);
+          searchUsers(event.target.value);
         }}
         onClick={() => {
           setShowSearchResults((prev) => !prev);
         }}
       />
-      <button className="absolute top-0 bottom-0 right-2" onClick={onSearch}>
+      <div className="absolute top-0 bottom-0 right-2">
         <CiSearch
           style={{
             color: 'white',
@@ -54,12 +63,29 @@ const SearchInput = () => {
             height: '1.8rem',
           }}
         />
-      </button>
+      </div>
       {showSearchResults && (
         <div
           className="w-full max-h-80 min-h-12 absolute  transform 
-      left-1/2 -translate-x-1/2 mt-2 shadow-lg bg-white/20 rounded-xl border border-white/20 "
-        ></div>
+      left-1/2 -translate-x-1/2 mt-2 shadow-lg bg-white/20 rounded-xl border border-white/20 flex flex-col overflow-y-auto p-2 z-10 "
+        >
+          {searchResultsUsers.map((user) => (
+            <div
+              key={user.id}
+              className="px-2 py-1 w-full text-white/80 hover:bg-white/20 hover:text-white"
+              onClick={() => {
+                router.push(`/profile/${user.username}`);
+              }}
+            >
+              <img
+                src={process.env.BACKEND + `/api/users/${user.id}/avatar`}
+                alt="avatar"
+                className="w-8 h-8 rounded-full inline-block mr-2"
+              />
+              <p>{user.username}</p>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
