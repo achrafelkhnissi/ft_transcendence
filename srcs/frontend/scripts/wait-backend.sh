fbc63d9e0daf6cfdccdb4ad3b745c1e27e72a4e4
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2024-02-21 11:13:29 +0100

    Update wait-backend.sh script to check if the NestJS application is running on the specified host

--- srcs/frontend/scripts/wait-backend.sh
+++ srcs/frontend/scripts/wait-backend.sh
@@ -6,9 +6,9 @@ host="$1"
 shift
 cmd="$@"
 
-# Check if the nestjs application is running on port 3000 before starting the frontend container
+# Check if the nestjs application is running before starting the frontend container
 until curl --output /dev/null --silent --head --fail "http://$host/api"; do
-  # >&2 echo "NestJS application is unavailable on port 3000 - sleeping"
+  >&2 echo "NestJS application is unavailable on $host - sleeping.."
   sleep 1
 done
 
