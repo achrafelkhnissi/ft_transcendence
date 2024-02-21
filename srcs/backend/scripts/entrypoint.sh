11a910e785b2ce6271347fb0c05aef41b91e725d
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2024-02-21 15:39:13 +0100

    Update Dockerfiles to their previous state.

--- srcs/backend/scripts/entrypoint.sh
+++ srcs/backend/scripts/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+set -e # Exit immediately if a command exits with a non-zero status.
+set -x # Print commands and their arguments as they are executed.
+
+# Install npm packages
+npm install
+
 # Wait for postgres to be ready
 until pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER
 do
@@ -15,7 +21,7 @@ if [ "$NODE_ENV" = "production" ]; then
   npx prisma migrate deploy
 else
   echo "Running in development mode"
-  npx prisma db push
+  npx prisma db push # --no-install # --skip-generate
 fi
 
 npx prisma studio --port 5555 &
