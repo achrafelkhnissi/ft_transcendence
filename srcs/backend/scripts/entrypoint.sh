1c6ddd16cf0d1f78752d369fd145d121eebb02fc
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2024-02-23 10:42:57 +0100

    Update entrypoint.sh script for backend

--- srcs/backend/scripts/entrypoint.sh
+++ srcs/backend/scripts/entrypoint.sh
@@ -4,7 +4,6 @@ set -e # Exit immediately if a command exits with a non-zero status.
 
 export PATH="/app/node_modules/.bin:$PATH"
 
-# Wait for postgres to be ready
 until pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER
 do
   echo "$(date) - waiting for database $POSTGRES_HOST:$POSTGRES_PORT..."
@@ -13,14 +12,14 @@ done
 
 >&2 echo "Postgres is up - continuing..."
 
-# TODO: Why does prisma is not installed? even after running `RUN npm install` in Dockerfile
 if [ "$NODE_ENV" = "production" ]; then
   echo "Running in production mode"
-  npx prisma migrate deploy
+  npx prisma generate
+  npm run build
 else
   ls -la
   echo "Running in development mode"
-  npx prisma db push # --no-install # --skip-generate
+  npx prisma db push
 fi
 
 npx prisma studio --port 5555 &
