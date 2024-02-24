038bf6891cb391313ccfc59e3431c9eb1f201952
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2024-02-24 13:55:54 +0100

    Fix: Backend prod build not working
    Add Prisma and NestJS CLI to Dockerfile, update entrypoint.sh

--- srcs/backend/scripts/entrypoint.sh
+++ srcs/backend/scripts/entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e # Exit immediately if a command exits with a non-zero status.
 
+if [ ! -d "node_modules" ]; then
+  echo "node_modules not found. Installing..."
+  npm install
+fi
+
 until pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER
 do
   echo "$(date) - waiting for database $POSTGRES_HOST:$POSTGRES_PORT..."
@@ -10,16 +15,13 @@ done
 
 >&2 echo "Postgres is up - continuing..."
 
+npx prisma db push
+
 if [ "$NODE_ENV" = "production" ]; then
   echo "Running in production mode"
   ls -la 
-  npx prisma migrate dev --name init
   npx prisma generate
   npx nest build
-else
-  echo "Running in development mode"
-  npm install # For hot reloading to work
-  npx prisma db push
 fi
 
 npx prisma studio --port 5555 &
