946b6dde2ca6c35e6f79a90813f28beacbcb8f47
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2024-02-24 11:14:03 +0100

    rm console.log

--- srcs/frontend/src/app/(site)/messages/[...id]/page.tsx
+++ srcs/frontend/src/app/(site)/messages/[...id]/page.tsx
@@ -238,7 +238,6 @@ const Home = ({ params }: { params: { id: number } }) => {
           case 'unmute':
           case 'join':
           case 'unban': {
-            console.log('muted ', res.data);
             if (res.data.type != 'DM') uppdateConversations(res.data);
             break;
           }
