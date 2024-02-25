159008aa284dff29c0e68ee12fe3127b9541debd
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2024-02-25 12:14:11 +0100

    Update Docker Compose and entrypoint script to work in dev and prod mode

--- srcs/backend/scripts/entrypoint.sh
+++ srcs/backend/scripts/entrypoint.sh
@@ -2,9 +2,10 @@
 
 set -e # Exit immediately if a command exits with a non-zero status.
 
-if [ ! -d "node_modules" ]; then
-  echo "node_modules not found. Installing..."
+if [ "$NODE_ENV" = "development" ]; then
+  echo "Installing dev dependencies..."
   npm install
+  echo "Dev dependencies installed."
 fi
 
 until pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER
@@ -16,14 +17,6 @@ done
 >&2 echo "Postgres is up - continuing..."
 
 npx prisma db push
-
-if [ "$NODE_ENV" = "production" ]; then
-  echo "Running in production mode"
-  ls -la 
-  npx prisma generate
-  npx nest build
-fi
-
 npx prisma studio --port 5555 &
 
 exec "$@"
