f5a76c2ce8b77b97bb3923bf894ba7c5466eea71
Fathiya Olatokunbo Jamia <fathiyajamaa051@gmail.com>
2024-02-26 17:05:14 +0100

    done achievements ui

--- srcs/frontend/src/components/userProfile/Achievements.tsx
+++ srcs/frontend/src/components/userProfile/Achievements.tsx
@@ -2,7 +2,9 @@ import AwsomeGame from '../achievements/AwsomeGame';
 import GoodGame from '../achievements/GoodGame';
 import Noob  from '../achievements/Noob';
 import ProGamer  from '../achievements/ProGamer';
+import LevelUp  from '../achievements/LevelUp';
 import Card from './Card';
+import Play from '../achievements/Play';
 import { GiAchievement } from 'react-icons/gi';
 
 const Achievements = () => {
@@ -11,11 +13,13 @@ const Achievements = () => {
       header="achievements"
       icon={<GiAchievement className="text-[#6C61A4] w-6 h-6 " />}
     >
-      <div className=' p-2 flex flex-wrap max-h-[280px] gap-3 justify-center'>
+      <div className=' p-6 flex flex-wrap max-h-[280px] gap-3 justify-center'>
         <Noob />
         <ProGamer />
         <GoodGame/>
         <AwsomeGame/>
+        <LevelUp/>
+        <Play/>
       </div>
     </Card>
   );
