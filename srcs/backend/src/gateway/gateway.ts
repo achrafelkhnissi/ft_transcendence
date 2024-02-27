0fd89c2f284422af7d552b95d0e822a953afd80f
Zaineb Sarir <zsarir@e2r8p6.1337.ma>
2024-02-27 20:47:29 +0100

    update

--- srcs/backend/src/gateway/gateway.ts
+++ srcs/backend/src/gateway/gateway.ts
@@ -111,7 +111,7 @@ export class Gateway
     console.log(`${user.username} disconnected`);
 
      //remove from the game
-     this.gameService.removeUserById(user.id);
+    this.gameService.removeUserById(user.id);
 
     // Check if room user.username is empty
     // If it is, then the user has no more sockets connected
