c52385750538205f423d18edd05aadda881f1a19
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2024-03-01 17:43:50 +0100

    Fix clip-path attribute in SVG paths

--- srcs/frontend/src/app/page.tsx
+++ srcs/frontend/src/app/page.tsx
@@ -133,22 +133,22 @@ export default function Home() {
                       <g transform="matrix(.727273 0 0 .727273 -.954545 -1.45455)">
                         <path
                           d="M0 37V11l17 13z"
-                          clip-path="url(#B)"
+                          clipPath="url(#B)"
                           fill="#fbbc05"
                         />
                         <path
                           d="M0 11l17 13 7-6.1L48 14V0H0z"
-                          clip-path="url(#B)"
+                          clipPath="url(#B)"
                           fill="#ea4335"
                         />
                         <path
                           d="M0 37l30-23 7.9 1L48 0v48H0z"
-                          clip-path="url(#B)"
+                          clipPath="url(#B)"
                           fill="#34a853"
                         />
                         <path
                           d="M48 48L17 24l-4-3 35-10z"
-                          clip-path="url(#B)"
+                          clipPath="url(#B)"
                           fill="#4285f4"
                         />
                       </g>
